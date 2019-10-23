db4a088ab3d9d0ad1cdb52e5fa191c1bbc8b2036
Hemant Nirmalkar <hemantnirmalkar@Hemants-MacBook-Air.local>
2019-10-23 19:58:20 +0530

    importer pokegame component instead of pokedex

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-import PokeDex from './PokeDex'
+import PokeGame from './PokeGame'
 import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <PokeDex />
+        <PokeGame />
       </div>
     )
   }
