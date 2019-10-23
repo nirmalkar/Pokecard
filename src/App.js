50dcee81ec5dd0dc4850288b1f2809f10a08a886
Hemant Nirmalkar <hemantnirmalkar@Hemants-MacBook-Air.local>
2019-10-23 15:04:41 +0530

    imported pokedex in the place of pokecard

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react'
-import PokeCard from './PokeCard'
+import PokeDex from './PokeDex'
 import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <PokeCard
-          id={4}
-          name='Charmander'
-          type='fire'
-          exp={62}
-        />
+        <PokeDex />
       </div>
     )
   }
