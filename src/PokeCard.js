bcc6c15a64383b1524ca1cd2504c68392d5720e5
Hemant Nirmalkar <hemantnirmalkar@Hemants-MacBook-Air.local>
2019-10-23 16:24:32 +0530

    added new api link for better quality images

--- src/PokeCard.js
+++ src/PokeCard.js
@@ -3,11 +3,14 @@ import React, { Component } from 'react'
 import './PokeCard.css'
 
 
-const POKE_API = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/`
+// const POKE_API = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/`
+const POKE_API = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/`
+let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number)
+
 
 class PokeCard extends Component {
   render() {
-    let imgSrc = `${POKE_API}${this.props.id}.png`;
+    let imgSrc = `${POKE_API}${padToThree(this.props.id)}.png`;
     return (
       <div className='PokeCard'>
         <h1 className='PokeCard-title'>{this.props.name}{this.props.id}</h1>
