674f67f12c607f8de9e04fbc49bf5de264432100
Hemant Nirmalkar <hemantnirmalkar@Hemants-MacBook-Air.local>
2019-10-23 20:31:39 +0530

    calculating experience using reduce method, for choosing winner

--- src/PokeGame.js
+++ src/PokeGame.js
@@ -23,12 +23,13 @@ export default class PokeGame extends Component {
       let randPokemon = hand2.splice(randIdx, 1)[0]
       hand1.push(randPokemon)
     }
-    console.log(hand1)
-    console.log(hand2)
+    let exp1 = hand1.reduce((exp, pokemon) => exp + pokemon.base_experience, 0)
+    let exp2 = hand2.reduce((exp, pokemon) => exp + pokemon.base_experience, 0)
+
     return (
       <div>
-        <PokeDex pokemon={hand1} />
-        <PokeDex pokemon={hand2} />
+        <PokeDex pokemon={hand1} exp={exp1} isWinner={exp1 > exp2} />
+        <PokeDex pokemon={hand2} exp={exp2} isWinner={exp2 > exp1} />
       </div>
     )
   }
