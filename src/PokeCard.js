c4d40ed55badacb37eccd617540e2d9459b25bf4
Hemant Nirmalkar <hemantnirmalkar@Hemants-MacBook-Air.local>
2019-10-23 15:06:25 +0530

    added pokecards for the card display of pokemons

--- src/PokeCard.js
+++ src/PokeCard.js
@@ -1,5 +1,6 @@
 
 import React, { Component } from 'react'
+import './PokeCard.css'
 
 
 const POKE_API = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/`
@@ -8,9 +9,11 @@ class PokeCard extends Component {
   render() {
     let imgSrc = `${POKE_API}${this.props.id}.png`;
     return (
-      <div className='PokCard'>
+      <div className='PokeCard'>
         <h1>{this.props.name}{this.props.id}</h1>
         <img src={imgSrc} alt={this.props.name} />
+        <div>Type: {this.props.type}</div>
+        <div>Exp: {this.props.exp}</div>
       </div>
     )
   }
